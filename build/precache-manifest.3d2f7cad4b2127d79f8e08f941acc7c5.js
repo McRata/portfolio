ef5898d936eef475d7b6271b858b974147f57b26
McRata <gtc0593@gmail.com>
2021-02-28 19:47:01 +0100

    Test deploy

---
+++ build/precache-manifest.3d2f7cad4b2127d79f8e08f941acc7c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e591f332a590e666868c",
+    "url": "/static/css/main.fe86d6d7.chunk.css"
+  },
+  {
+    "revision": "e591f332a590e666868c",
+    "url": "/static/js/main.d9760d55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c77bf12f0bdd9b8eeb55",
+    "url": "/static/js/2.b584dd27.chunk.js"
+  },
+  {
+    "revision": "2fa2b2245f2b3c1e7c6e281703fc5e86",
+    "url": "/index.html"
+  }
+];
