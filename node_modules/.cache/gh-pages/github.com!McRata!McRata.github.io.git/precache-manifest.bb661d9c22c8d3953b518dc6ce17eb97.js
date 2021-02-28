7fc85ddec5c7c50f025c06a1c5b8d4721728d65d
McRata <gtc0593@gmail.com>
2021-02-28 19:55:29 +0100

    Test deploy

---
+++ node_modules/.cache/gh-pages/github.com!McRata!McRata.github.io.git/precache-manifest.bb661d9c22c8d3953b518dc6ce17eb97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15ba1efaa399a5bd4d75",
+    "url": "/myapp/static/css/main.fe86d6d7.chunk.css"
+  },
+  {
+    "revision": "15ba1efaa399a5bd4d75",
+    "url": "/myapp/static/js/main.d2146673.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "c77bf12f0bdd9b8eeb55",
+    "url": "/myapp/static/js/2.b584dd27.chunk.js"
+  },
+  {
+    "revision": "9bba62ab11bba91980a3b9c43e75ce85",
+    "url": "/myapp/index.html"
+  }
+];
