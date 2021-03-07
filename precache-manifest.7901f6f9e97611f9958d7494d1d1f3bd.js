f5f555001fc4b6a9971ecca0acedbbd24d0f3d1c
McRata <gtc0593@gmail.com>
2021-03-07 20:56:59 +0100

    Updates

---
+++ precache-manifest.7901f6f9e97611f9958d7494d1d1f3bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "955a5c9c533480dac2ed",
+    "url": "/portfolio/static/css/main.fe86d6d7.chunk.css"
+  },
+  {
+    "revision": "955a5c9c533480dac2ed",
+    "url": "/portfolio/static/js/main.6c958fe3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c77bf12f0bdd9b8eeb55",
+    "url": "/portfolio/static/js/2.b584dd27.chunk.js"
+  },
+  {
+    "revision": "fbce6311891b9cfaf1786360a9da287c",
+    "url": "/portfolio/index.html"
+  }
+];
